04a835b75bc45dc034a759f5c08eda89b6840bf7
Mark Vincze <mrk.vincze@gmail.com>
2019-04-22 12:36:28 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace GhostHugoMigrator
+{
+    class Program
+    {
+        private const string jsonExportPath = @"C:\Users\mvincze\Google Drive\Blog\Vultr\Backups\2019-04-14\mark-vinczes-coding-blog.ghost.2019-04-14-trimmed.json";
+
+        private const string hugoPostsFolder = @"C:\Workspaces\Github\markvincze-blog\content\posts";
+
+        static void Main(string[] args)
+        {
+            var json = File.ReadAllText(jsonExportPath);
+            var jobj = JObject.Parse(json);
+
+            var posts = jobj["db"][0]["data"]["posts"] as JArray;
+
+            Console.WriteLine("Number of posts: {0}", posts.Count);
+        }
+    }
+}
